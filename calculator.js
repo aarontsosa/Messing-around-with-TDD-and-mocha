fe658c1d75ac79b9efe9af1d9c1a103a649640ae
Aaron Sosa <aarontsosa@gmail.com>
2017-08-07 14:55:18 -0400

    writing more to TDD for chai and mocha for the calculator

--- calculator.js
+++ calculator.js
@@ -3,8 +3,25 @@ class Calculator {
 
 
     }
+
     add(num1, num2){
-        
+        return num1 + num2;
+    }
+
+    sub(num1, num2){
+        return num1 - num2;
+    }
+
+    multiply(num1, num2){
+        return num1 * num2;
+    }
+
+    divide(num1, num2){
+        return num1 / num2;
+    }
+
+    addAsync(num1, num2, callback) {
+        callback(num1 + num2)
     }
 }
 
--- test/test.js
+++ test/test.js
@@ -8,6 +8,53 @@ describe('Can do Arithmetic', ()=> {
         it('Can add two positive numbes', () =>{
             expect(calc.add(1,1)).to.equal(2);
         })
+        it('Can add to zero', () => {
+            expect(calc.add(1, 0)).to.equal(1)
+        })
+        it('can add negative numbers', () => {
+            expect(calc.add(-2, -5)).to.equal(-7)
+        })
+    })
+    describe('Can Subtract two numbers', () => {
+        it('Can add two positive numbers', () =>{
+            expect(calc.sub(1,1)).to.equal(0)
+        })
+        it('Can subtract from zero', () =>{
+            expect(calc.sub(0, 3)).to.equal(-3)
+        })
+        it('Can subtrat two negative numbers', () => {
+            expect(calc.sub(-4, -5)).to.equal(1)
+        })
+    })
+    describe('Can divide two numbers', () => {
+        it('can divide two positive numbers', () => {
+            expect(calc.divide(1, 1)).to.equal(1)
+        })
+        it('can divide 0 into itself', () =>{
+            expect(calc.divide(1, 0)).to.equal(Infinity)
+        })
+        it('can divide two negative numbers', () =>{
+            expect(calc.divide(-4, -2)).to.equal(2)
+        })
+    })
+    describe('Can mutliply two numbers', () =>{
+        it('can multi two positive numbers', () =>{
+            expect(calc.multiply(2, 2)).to.equal(4)
+        })
+    })
+    describe('can add numbers asynchronously', () =>{
+        it('can add two integers asynchronusly', (done) => {
+            calc.addAsync(3, 2, (val) => {
+                if (val === 5){
+                    done();
+                } else {
+                    done(new Error('nope That is not right'))
+                }
+            })
+            // expect(calc.addAsync(1, 1, (val) =>{
+
+            // }).to.equal(5))
+        })
     })
 })
 // describe('Matching numbers', () => {
