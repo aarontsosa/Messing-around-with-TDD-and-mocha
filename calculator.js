c626e4823f3072348170e611b39d35d0d1c0e74b
Aaron Sosa <aarontsosa@gmail.com>
2017-08-07 14:23:04 -0400

    working on calculator JS using TDD

---
+++ calculator.js
@@ -0,0 +1,11 @@
+class Calculator {
+    constructor(){
+
+
+    }
+    add(num1, num2){
+        
+    }
+}
+
+module.exports = Calculator
--- test/test.js
+++ test/test.js
@@ -1,5 +1,18 @@
-const exepct = require('chai').expect;
+const expect = require('chai').expect;
+const Calculator = require('../calculator');
 
-describe('taking a first stab at TDD', () => {
-    exepct(1).to.equal(1);
+let calc = new Calculator();
+
+describe('Can do Arithmetic', ()=> {
+    describe('Can add two positive integers', () => {
+        it('Can add two positive numbes', () =>{
+            expect(calc.add(1,1)).to.equal(2);
+        })
+    })
 })
+// describe('Matching numbers', () => {
+//     it('does 1 === 1', () => {
+//         expect(1).to.equal(1);
+//         expect(2).to.equal(1);
+//     })
+// })
